c030c0326e13afd719e67c1c11dfb872b98f4382
yudy <ananda@hobbious.com>
2022-10-23 15:43:45 +0700

    length not 0

--- src/pages/premade-data-atom.tsx
+++ src/pages/premade-data-atom.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { NextPage } from "next";
-import { atom, useAtom, PrimitiveAtom } from 'jotai'
-import { splitAtom } from 'jotai/utils'
+import { atom, useAtom, PrimitiveAtom } from "jotai";
+import { splitAtom } from "jotai/utils";
 import { ChangeEvent } from "react";
 
 interface Heroes {
-  name: string
+  name: string;
 }
 
 interface NewHeroes extends Heroes {
@@ -14,45 +14,45 @@ interface NewHeroes extends Heroes {
 
 const allHeroes = [
   {
-    name: 'Batman',
+    name: "Batman",
   },
   {
-    name: 'Superman',
+    name: "Superman",
   },
   {
-    name: 'Wonder Woman',
+    name: "Wonder Woman",
   },
-]
+];
 
-const newHeroes = allHeroes.map(item => ({...item, isChecked: false}))
-const dataAtom = atom<NewHeroes[]>(newHeroes)
-const heroesAtomsAtom = splitAtom(dataAtom)
-const selectedHeroes = atom<string[]>([])
+const newHeroes = allHeroes.map((item) => ({ ...item, isChecked: false }));
+const dataAtom = atom<NewHeroes[]>(newHeroes);
+const heroesAtomsAtom = splitAtom(dataAtom);
+const selectedHeroes = atom<string[]>([]);
 
-const Hero = ({hero}: {hero: PrimitiveAtom<NewHeroes>}) => {
-  const [data, setData]  = useAtom(hero)
-  const [list, setList] = useAtom(selectedHeroes)
+const Hero = ({ hero }: { hero: PrimitiveAtom<NewHeroes> }) => {
+  const [data, setData] = useAtom(hero);
+  const [list, setList] = useAtom(selectedHeroes);
 
   function handleChecklist(e: ChangeEvent<HTMLInputElement>) {
-    let updatedList = [...list]
+    let updatedList = [...list];
     const { checked, value } = e.target;
     const isInclude = updatedList.includes(value);
     if (checked) {
       if (isInclude) updatedList = [...list];
-      if (!isInclude) updatedList = [...list, value]
+      if (!isInclude) updatedList = [...list, value];
     }
     if (!checked) {
       updatedList.splice(list.indexOf(value), 1);
     }
 
-    const checkState = updatedList.includes(data.name)
+    const checkState = updatedList.includes(data.name);
 
     setList(updatedList);
-    setData({...data, isChecked: checkState})
+    setData({ ...data, isChecked: checkState });
   }
 
   return (
-    <li className="flex gap-2 justify-center">
+    <li className="flex justify-center gap-2">
       <div>{data.name}</div>
       <input
         type="checkbox"
@@ -61,28 +61,24 @@ const Hero = ({hero}: {hero: PrimitiveAtom<NewHeroes>}) => {
         checked={data.isChecked}
       />
     </li>
-  )
-}
-
+  );
+};
 
 const PremadeDataAtomPage: NextPage = () => {
-  const [heroes] = useAtom(heroesAtomsAtom)
-  const [list] = useAtom(selectedHeroes)
+  const [heroes] = useAtom(heroesAtomsAtom);
+  const [list] = useAtom(selectedHeroes);
 
   return (
-    <div className="container min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-2xl mb-4 font-semibold">Heroes</h1>
+    <div className="container flex min-h-screen flex-col items-center justify-center">
+      <h1 className="mb-4 text-2xl font-semibold">Heroes</h1>
       <ul className="text-center leading-loose">
-        {heroes.map(hero => (
+        {heroes.map((hero) => (
           <Hero key={hero as any} hero={hero} />
         ))}
       </ul>
-      <div>
-        {JSON.stringify(list)}
-      </div>
+      <div>{list.length !== 0 && JSON.stringify(list)}</div>
     </div>
-  )
+  );
 };
 
 export default PremadeDataAtomPage;
-
