4da5f68e6cfddce9844117cfc38d40082d4a03c8
yudy <ananda@hobbious.com>
2022-10-23 16:24:03 +0700

    rename

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,8 @@ const Home: NextPage = () => {
   return (
     <div className="container flex min-h-screen flex-col items-center justify-center">
       <div className="mb-10 flex gap-10 underline">
-        <Link href="/hardcoded">Premade Data</Link>
-        <Link href="/trpc">tRPC Atom</Link>
+        <Link href="/hardcoded">Hard Coded</Link>
+        <Link href="/trpc">tRPC</Link>
       </div>
       <h1 className="mb-4 text-2xl font-semibold">Jotai SplitAtom</h1>
     </div>
